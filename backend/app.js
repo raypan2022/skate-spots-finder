0dfcf593a3f0a894cac8a65f658af4d39f034989
Raymond Pan <raymondpan2022@gmail.com>
2023-12-22 15:59:27 -0500

    added location.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
